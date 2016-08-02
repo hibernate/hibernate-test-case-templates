716b932406ab5dee424a98c2df8d7e3357c9ae6e
Chris Cranford <chris@hibernate.org>
2016-08-02 15:57:23 -0500

    Add Envers 5.x test case template.

---
+++ envers/envers-5/src/test/java/org/hibernate/bugs/EnversUnitTestCase.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2014 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hibernate.bugs;
+
+import org.hibernate.cfg.AvailableSettings;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.envers.AuditReader;
+import org.junit.Test;
+
+/**
+ * This template demonstrates how to develop a test case for Hibernate Envers, using
+ * its built-in unit test framework.
+ */
+public class EnversUnitTestCase extends AbstractEnversTestCase {
+
+	// Add your entities here.
+	@Override
+	protected Class[] getAnnotatedClasses() {
+		return new Class[] {
+//				Foo.class,
+//				Bar.class
+		};
+	}
+
+	// If you use *.hbm.xml mappings, instead of annotations, add the mappings here.
+	@Override
+	protected String[] getMappings() {
+		return new String[] {
+//				"Foo.hbm.xml",
+//				"Bar.hbm.xml"
+		};
+	}
+	// If those mappings reside somewhere other than resources/org/hibernate/test, change this.
+	@Override
+	protected String getBaseForMappings() {
+		return "org/hibernate/test/";
+	}
+
+	// Add in any settings that are specific to your test.  See resources/hibernate.properties for the defaults.
+	@Override
+	protected void configure(Configuration configuration) {
+		super.configure( configuration );
+
+		configuration.setProperty( AvailableSettings.SHOW_SQL, Boolean.TRUE.toString() );
+		configuration.setProperty( AvailableSettings.FORMAT_SQL, Boolean.TRUE.toString() );
+		//configuration.setProperty( AvailableSettings.GENERATE_STATISTICS, "true" );
+	}
+
+	// Add your tests, using standard JUnit.
+	@Test
+	public void hhh123Test() throws Exception {
+		AuditReader reader = getAuditReader();
+		// Do stuff...
+	}
+}
