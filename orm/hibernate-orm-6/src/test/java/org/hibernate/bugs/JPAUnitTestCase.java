fd5a5633a31fd0db84906768b17ed6ddcec2f046
thjanssen <thorben@thoughts-on-java.org>
2022-04-27 17:53:55 +0200

    Add missing test case and update Log4J

--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,17 +1,25 @@
 package org.hibernate.bugs;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
  */
 public class JPAUnitTestCase {
+
+    Logger log = LogManager.getLogger(this.getClass().getName());
 
 	private EntityManagerFactory entityManagerFactory;
 
@@ -28,10 +36,20 @@ public class JPAUnitTestCase {
 	// Entities are auto-discovered, so just add them anywhere on class-path
 	// Add your tests, using standard JUnit.
 	@Test
-	public void hhh123Test() throws Exception {
+	public void notFoundIssue() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+		
+        ChessGame game = entityManager.find(ChessGame.class, 1L);
+        assertNotNull("Returned entity shouldn't be null", game);
+        assertNull(game.getPlayerBlack(), "Broken foreign key reference with NotFoundAction.IGNORE should return null");
+        try {
+            game.getPlayerWhite();
+            fail("Accessing a broken foreign key reference should throw an exception.");
+        } catch (Exception e) {
+            // Exception expected
+        }
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
 	}
