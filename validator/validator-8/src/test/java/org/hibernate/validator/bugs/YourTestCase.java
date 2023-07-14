1d824d6b03801893f210e62308c21cdc2b8107ad
marko-bekhta <marko.prykladna@gmail.com>
2023-07-14 09:09:44 +0200

    Add a template for Hibernate Validator 8

---
+++ validator/validator-8/src/test/java/org/hibernate/validator/bugs/YourTestCase.java
@@ -0,0 +1,38 @@
+package org.hibernate.validator.bugs;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Set;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+
+import org.hibernate.validator.testutil.TestForIssue;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class YourTestCase {
+
+	private static Validator validator;
+
+	@BeforeClass
+	public static void setUp() {
+		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+		validator = factory.getValidator();
+	}
+
+	@Test
+	@TestForIssue(jiraKey = "HV-NNNNN") // Please fill in the JIRA key of your issue
+	public void testYourBug() {
+		YourAnnotatedBean yourEntity1 = new YourAnnotatedBean( null, "example" );
+
+		Set<ConstraintViolation<YourAnnotatedBean>> constraintViolations = validator.validate( yourEntity1 );
+		assertEquals( 1, constraintViolations.size() );
+		assertEquals(
+				"must not be null",
+				constraintViolations.iterator().next().getMessage() );
+	}
+
+}
