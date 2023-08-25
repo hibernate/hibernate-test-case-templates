49b7b140d0e9adee22e0291ec0f25112dee688e9
Edgar Asatryan <nstdio@gmail.com>
2023-08-25 18:41:51 +0400

    Attribute::getJavaType gets incorrect class

--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,9 +1,16 @@
 package org.hibernate.bugs;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import io.hypersistence.utils.hibernate.type.array.ListArrayType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Id;
 import jakarta.persistence.Persistence;
-
+import java.util.List;
+import org.hibernate.annotations.Type;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -28,11 +35,22 @@ public class JPAUnitTestCase {
 	// Entities are auto-discovered, so just add them anywhere on class-path
 	// Add your tests, using standard JUnit.
 	@Test
-	public void hhh123Test() throws Exception {
+	public void hhh17134Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
-		entityManager.getTransaction().begin();
-		// Do stuff...
-		entityManager.getTransaction().commit();
+		var entity = entityManager.getMetamodel().managedType(Example.class);
+
+		assertEquals(List.class, entity.getAttribute("list").getJavaType());
+
 		entityManager.close();
+	}
+	
+	@Entity
+	static class Example {
+		@Id
+		private int id;
+		
+		@Column(columnDefinition = "INTEGER ARRAY")
+		@Type(ListArrayType.class)
+		private List<Integer> list;
 	}
 }
