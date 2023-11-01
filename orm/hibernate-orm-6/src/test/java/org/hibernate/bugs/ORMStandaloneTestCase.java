929588314787260f9f799e190f7a465a8d1bf30d
Ruslan Hryn <hryn.external@crxmarkets.com>
2023-11-01 14:42:27 +0200

    Updated reproducer

--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/ORMStandaloneTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/ORMStandaloneTestCase.java
@@ -53,7 +53,7 @@ public class ORMStandaloneTestCase {
         session.clear();
         Transaction newTx = session.beginTransaction();
         List<Long> result = session.createQuery(
-                "select related.id from Main m right join m.related as related",
+                "select related.id from Main m right join m.related as related where related is not null",
                 Long.class).getResultList();
         Assertions.assertEquals(1, result.get(0));
         newTx.commit();
