a4bb1743d0b601d8b233254ac99d361de6923a09
marko-bekhta <marko.prykladna@gmail.com>
2024-08-12 18:34:34 +0200

    Convert ORM 6 test templates to use JUnit 5
    and all cool new JUnit extensions that we have in ORM ;)

--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/ORMStandaloneTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/ORMStandaloneTestCase.java
@@ -4,37 +4,37 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.junit.Before;
-import org.junit.Test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 /**
  * This template demonstrates how to develop a standalone test case for Hibernate ORM.  Although this is perfectly
  * acceptable as a reproducer, usage of ORMUnitTestCase is preferred!
  */
-public class ORMStandaloneTestCase {
+class ORMStandaloneTestCase {
 
 	private SessionFactory sf;
 
-	@Before
-	public void setup() {
+	@BeforeEach
+	void setup() {
 		StandardServiceRegistryBuilder srb = new StandardServiceRegistryBuilder()
-			// Add in any settings that are specific to your test. See resources/hibernate.properties for the defaults.
-			.applySetting( "hibernate.show_sql", "true" )
-			.applySetting( "hibernate.format_sql", "true" )
-			.applySetting( "hibernate.hbm2ddl.auto", "update" );
+				// Add in any settings that are specific to your test. See resources/hibernate.properties for the defaults.
+				.applySetting( "hibernate.show_sql", "true" )
+				.applySetting( "hibernate.format_sql", "true" )
+				.applySetting( "hibernate.hbm2ddl.auto", "update" );
 
 		Metadata metadata = new MetadataSources( srb.build() )
-		// Add your entities here.
-		//	.addAnnotatedClass( Foo.class )
-			.buildMetadata();
+				// Add your entities here.
+				//	.addAnnotatedClass( Foo.class )
+				.buildMetadata();
 
 		sf = metadata.buildSessionFactory();
 	}
 
-	// Add your tests, using standard JUnit.
-
+	// Add your tests, using standard JUnit 5:
 	@Test
-	public void hhh123Test() throws Exception {
+	void hhh123Test() throws Exception {
 
 	}
 }
