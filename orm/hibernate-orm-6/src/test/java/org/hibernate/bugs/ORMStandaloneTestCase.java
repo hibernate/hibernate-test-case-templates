e0c52e94e821832c771bfaae8578dc24bd7ecba5
Sven Strickroth <email@cs-ware.de>
2024-08-18 19:16:46 +0200

    Simplified

--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/ORMStandaloneTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/ORMStandaloneTestCase.java
@@ -6,8 +6,6 @@ import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.bugs.datamodel.ATest;
-import org.hibernate.bugs.datamodel.Submission;
-import org.hibernate.bugs.datamodel.Task;
 import org.hibernate.bugs.datamodel.TestCount;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -25,8 +23,6 @@ class ORMStandaloneTestCase {
 
 		Metadata metadata = new MetadataSources(srb.build())
 				// Add your entities here.
-				.addAnnotatedClass(Task.class)
-				.addAnnotatedClass(Submission.class)
 				.addAnnotatedClass(ATest.class)
 				.addAnnotatedClass(TestCount.class)
 				.buildMetadata();
@@ -37,21 +33,9 @@ class ORMStandaloneTestCase {
 	@Test
 	void hhh18501Test() throws Exception {
 		Session session = sf.openSession();
-		{
-			session.getTransaction().begin();
-			Task task = new Task();
-			session.persist(task);
-			session.persist(new Submission(task));
-			session.getTransaction().commit();
-		}
-		Submission submission = session.get(Submission.class, 1);
 		session.getTransaction().begin();
 		ATest test = new ATest();
-		test.setTask(submission.getTask());
 		session.persist(test);
-		session.getTransaction().commit();
-		session.getTransaction().begin();
-		test.setValue(5);
 		session.getTransaction().commit();
 		{ // as soon as this block is inserted, we get an IllegalStateException at the last commit
 			session.getTransaction().begin();
--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/datamodel/ATest.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/datamodel/ATest.java
@@ -6,8 +6,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -18,10 +16,6 @@ public class ATest implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	@ManyToOne
-	@JoinColumn(name = "taskid", nullable = false)
-	private Task task;
-	private int value = 0;
 
 	/**
 	 * @return the id
@@ -35,33 +29,5 @@ public class ATest implements Serializable {
 	 */
 	public void setId(int id) {
 		this.id = id;
-	}
-
-	/**
-	 * @return the task
-	 */
-	public Task getTask() {
-		return task;
-	}
-
-	/**
-	 * @param task the task to set
-	 */
-	public void setTask(Task task) {
-		this.task = task;
-	}
-
-	/**
-	 * @return the value
-	 */
-	public int getValue() {
-		return value;
-	}
-
-	/**
-	 * @param value the value to set
-	 */
-	public void setValue(int value) {
-		this.value = value;
 	}
 }
