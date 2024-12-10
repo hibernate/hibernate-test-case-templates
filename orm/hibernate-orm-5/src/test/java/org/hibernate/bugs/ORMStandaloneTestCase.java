607c768f62b96d246ea5eeeb7818c92a72421850
Kamil Pociot <kamil.pociot@auto1.com>
2024-12-10 09:19:00 +0100

    6.5.1 test

---
+++ orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/ORMStandaloneTestCase.java
@@ -0,0 +1,40 @@
+package org.hibernate.bugs;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * This template demonstrates how to develop a standalone test case for Hibernate ORM.  Although this is perfectly
+ * acceptable as a reproducer, usage of ORMUnitTestCase is preferred!
+ */
+public class ORMStandaloneTestCase {
+
+	private SessionFactory sf;
+
+	@Before
+	public void setup() {
+		StandardServiceRegistryBuilder srb = new StandardServiceRegistryBuilder()
+			// Add in any settings that are specific to your test. See resources/hibernate.properties for the defaults.
+			.applySetting( "hibernate.show_sql", "true" )
+			.applySetting( "hibernate.format_sql", "true" )
+			.applySetting( "hibernate.hbm2ddl.auto", "update" );
+
+		Metadata metadata = new MetadataSources( srb.build() )
+		// Add your entities here.
+		//	.addAnnotatedClass( Foo.class )
+			.buildMetadata();
+
+		sf = metadata.buildSessionFactory();
+	}
+
+	// Add your tests, using standard JUnit.
+
+	@Test
+	public void hhh123Test() throws Exception {
+
+	}
+}
