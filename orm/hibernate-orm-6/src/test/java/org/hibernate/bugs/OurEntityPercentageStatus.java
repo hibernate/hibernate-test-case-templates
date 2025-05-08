2ce9669a3ba87c31d23f8394d38d78d1b5c2213e
Stefan Schilling <Stefan.Schilling@EXXETA.com>
2025-05-08 09:26:21 +0200

    created test case using pure Hibernate

---
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/OurEntityPercentageStatus.java
@@ -0,0 +1,28 @@
+package org.hibernate.bugs;
+
+public class OurEntityPercentageStatus {
+
+    private String commonName;
+    private Double percentageStatus;
+
+    public OurEntityPercentageStatus(String commonName, Double percentageStatus) {
+        this.commonName = commonName;
+        this.percentageStatus = percentageStatus;
+    }
+
+    public String getCommonName() {
+        return commonName;
+    }
+
+    public void setCommonName(String commonName) {
+        this.commonName = commonName;
+    }
+
+    public Double getPercentageOut() {
+        return percentageStatus;
+    }
+
+    public void setPercentageOut(Double percentageOut) {
+        this.percentageStatus = percentageOut;
+    }
+}
